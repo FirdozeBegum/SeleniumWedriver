9e04a4a910003db00e0e856aa2c95cce6a454094
FirdozeBegum <125542411+FirdozeBegum@users.noreply.github.com>
2025-03-23 14:19:11 +0530

    Third commit

--- src/test/java/Selenium/Css_Locators.java
+++ src/test/java/Selenium/Css_Locators.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Css_Locators {
-//firdoze
+
 	public static void main(String[] args) {
 
 		WebDriver driver = new ChromeDriver();
